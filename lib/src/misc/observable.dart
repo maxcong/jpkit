fd7a99ba7e7297e8bfdb4e068d53eb6d53d75aec
Max.cong <congyushuai@gmail.com>
2019-01-18 12:53:38 +0800

    改良Observable可以携带Data，简化包装ViewModel时的代码量

--- lib/src/misc/observable.dart
+++ lib/src/misc/observable.dart
@@ -1,9 +1,11 @@
 import 'package:rxdart/rxdart.dart';
 
 class JPObservable<T> {
+  T data;
   Subject<T> sink;
   Observable stream;
-  JPObservable() {
+  JPObservable(T initData) {
+    data = initData;
     sink = BehaviorSubject<T>();
     stream = Observable(sink.stream);
   }
