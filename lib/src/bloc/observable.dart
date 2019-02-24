dc0e5d4b2d4727565881db4464a65f50374fdfa8
Max.cong <congyushuai@gmail.com>
2019-02-24 11:42:26 +0800

    更新observable

--- lib/src/bloc/observable.dart
+++ lib/src/bloc/observable.dart
@@ -3,11 +3,10 @@ import 'package:rxdart/rxdart.dart';
 class JPObservable<T> {
   T data;
   Subject<T> sink;
-  Observable stream;
+  Observable get stream => sink.stream;
   JPObservable(T initData) {
     data = initData;
     sink = BehaviorSubject<T>();
-    stream = Observable(sink.stream);
   }
   dispost() async {
     try {
